98aaf6518c66673b9e0d645661e0ded8b6259d98
Amit Kumar Goswami <cseiubatgoswami@gmail.com>
2022-12-04 23:28:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from './Pages/HomePage';
+import AdminPage from './Pages/AdminPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div className='d-flex flex-column min-vh-100'>
+        <header>
+          <Navbar bg="dark" variant='dark' expand="lg">
+            <Container>
+              <Navbar.Brand>Support Desk</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className='me-auto w-100 justify-content-end'>
+                      <a href="/admin" className='nav-link' target="_blank">Admin</a>
+                    </Nav>
+                  </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+            <Container className='mt-3'>
+              <Routes>
+                <Route path="/" element={<HomePage />}></Route>
+                <Route path='/admin' element={<AdminPage />}></Route>
+              </Routes>
+            </Container>
+        </main>
+        <footer className='mt-auto'>
+          <div className='text-center'>All rights Reserved.</div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatBox from '../Components/ChatBox'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>Home Page</h1>
+        <p>Your website content</p>
+        <ChatBox />
+    </div>
+  )
+}
+
+export default HomePage
